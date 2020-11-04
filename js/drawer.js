ec880034635072943cc1a93b7ee12f60dd921964
Alexis <zorrillaalvites@gmail.com>
2020-11-04 19:29:34 -0300

    v2

--- js/drawer.js
+++ js/drawer.js
@@ -1,4 +1,24 @@
 var dibujador = function() {
 	var map = mapCreation('mapid');
 }
-$(dibujador);
+$(dibujador);					
+
+function buscar(){
+	var nombre = document.getElementById("nombre").value;	
+	var autor = document.getElementById("autor").value;
+	var tipo = document.getElementById("tipo").value;
+	traerDatos();
+	
+}
+function traerDatos(){
+	console.log("traerDatos");
+	const xhttp = new XMLHttpRequest();
+	xhttp.open('GET','cuadros.json',true);
+	xhttp.send();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText);
+		}
+	}
+
+}
